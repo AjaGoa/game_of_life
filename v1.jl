d41f11fb2e06f748083d9e1c19f3530ecc7dd489
AjaGoa <alinka27200@gmail.com>
2025-10-20 21:06:50 +0200

    OffsetM

--- v1.jl
+++ v1.jl
@@ -5,22 +5,36 @@ using OffsetArrays
 GLMakie.activate!(framerate = 15.0) 
 
 mutable struct Board
-    board::AbstractMatrix{Bool} # lepsi jenom Matrix nez AbstractMatrix - obecnejsi - jenom Matrix nefunguje - nepredava si spravne indexovani - moc hodnot
-    new_board::AbstractMatrix{Bool}# radsi udelat similar boardu 
+    board::OffsetMatrix{Bool} # AbstractMatrix je "nadrazena" Matrix/OffsetM/... 
+    new_board::OffsetMatrix{Bool}
 end
 
-# ruzne druhy konstruktoru
+# ruzne druhy konstruktoru: https://docs.julialang.org/en/v1/manual/constructors/ 
+mutable struct FigureParams
+    isrunning::Observable{Bool} 
+    current_gen::Observable{Int} # ve fig, pocitani generaci
+    board_plot::Observable{Matrix{Bool}}
+end
 
 function Board_init(x::Int, y::Int) # viditelne
-    
     board = OffsetArray(rand(Bool, x + 2, y + 2), 0:x + 1, 0:y + 1)
     new_board = similar(board)
     return Board(board, new_board)
 end
 
-function wrap_board(board::AbstractMatrix{Bool})
-    x, y = size(board) # x = row, y = col 
-    x, y = x - 2, y - 2
+function FigureParams(b::Board)
+    x, y = size(b.board) .- 2
+
+    isrunning = Observable(false)
+    current_gen = Observable(0)
+    board_plot = Observable(b.board[1:x, 1:y])
+
+    return FigureParams(isrunning, current_gen, board_plot)
+end
+
+function wrap_board!(board::OffsetMatrix{Bool})
+    x, y = size(board) .- 2# x = row, y = col 
+
     # edges
     board[0,    1:y] .= board[x, 1:y]    # top halo
     board[(x + 1),  1:y] .= board[1, 1:y]    # bottom halo
@@ -34,7 +48,7 @@ function wrap_board(board::AbstractMatrix{Bool})
     board[(x + 1), (y + 1)]  = board[1, 1]  # bottom-right corner
 end
 
-function print_board(b::AbstractMatrix{Bool})
+function print_board(b::OffsetMatrix{Bool})
     println("Next")
     for row in axes(b, 1) # misto 1:size, hodi mi zrovna zacatek:konec # print i s halo
         for col in axes(b, 2)
@@ -45,51 +59,29 @@ function print_board(b::AbstractMatrix{Bool})
 end
 
 function next_generation!(b::Board)
-    x, y = size(b.board)
-    x, y = x-2, y-2
+    x, y = size(b.board) .- 2
 
-    wrap_board(b.board)
-
+    wrap_board!(b.board)
     for i in 1:x, j in 1:y
         neighbors = sum(b.board[i-1:i+1, j-1:j+1]) - b.board[i, j]
         b.new_board[i, j] = (neighbors == 3 || (b.board[i, j] && neighbors == 2))
     end
-
     b.board, b.new_board = b.new_board, b.board # bez copy mi to jenom odkazuje na misto v pameti ale new_board, a to potom upravuju, takze tam bude delat bordel
 end
 
-mutable struct FigureParams
-    isrunning::Observable{Bool} 
-    current_gen::Observable{Int} # ve fig, pocitani generaci
-    board_plot::Observable{Matrix{Bool}}
-end
-# konstruktor
-function FigureParams(b::Board)
-    x, y = size(b.board)
-    x, y = x-2, y-2
-
-    isrunning = Observable(false)
-    current_gen = Observable(0)
-    board_plot = Observable(b.board[1:x, 1:y])
-
-    return FigureParams(isrunning, current_gen, board_plot)
-end
-
 function make_figure(b::Board, fp::FigureParams)
-
-    x, y = size(b.board)
-    x, y = x-2, y-2
+    x, y = size(b.board) .- 2
     
     fig = Figure(size = (700, 700)) # ! # lepsi pouzit dynamickou velikost cells
     gl = GridLayout(fig[1, 1], alignmode = Outside())
     ax = Axis(gl[1, 1], # span (1.0, 10.0) , here set up automatically based on the data I put in (board_plot), but can be set manually
-    aspect = DataAspect(),
-    title = "Game of Life",
-    xticklabelsvisible = false, yticklabelsvisible = false,
-    xticksvisible = false, yticksvisible = false)
+        aspect = DataAspect(),
+        title = "Game of Life",
+        xticklabelsvisible = false, yticklabelsvisible = false,
+        xticksvisible = false, yticksvisible = false)
     colsize!(gl, 1, Fixed(400)) 
     rowsize!(gl, 1, Fixed(400))
-    empty!(ax.interactions)
+    empty!(ax.interactions) # disable default interactions
     
     hm = heatmap!(ax, fp.board_plot, colormap = [:black, :white], colorrange = (0, 1))
     
@@ -108,7 +100,7 @@ function make_figure(b::Board, fp::FigureParams)
             fp.isrunning[] = !fp.isrunning[]
         end
     end
-    
+    # nesel by ten button a keyboard sloucit do jednoho eventu ?
     on(fig.scene.events.tick) do _
         fp.isrunning[] || return # if not running, do nothing
 
@@ -120,7 +112,6 @@ function make_figure(b::Board, fp::FigureParams)
     end
 
     on(reset_button.clicks) do _
-    
         fp.isrunning[] = false
         b.board = OffsetArray(rand(Bool, x + 2, y + 2), 0:x + 1, 0:y + 1)
         fp.board_plot[] = b.board[1:x, 1:y]
@@ -128,10 +119,8 @@ function make_figure(b::Board, fp::FigureParams)
     end
     
     on(clear_button.clicks) do _
-        
         fp.isrunning[] = false
         b.board = OffsetArray(falses(x + 2, y + 2), 0:x + 1, 0:y + 1)
-        
         fp.board_plot[] = b.board[1:x, 1:y]
         fp.current_gen[] = 0
     end
@@ -148,22 +137,13 @@ function make_figure(b::Board, fp::FigureParams)
             end
         end
     end
-
     display(fig)
 end
 
-# step buttonek
-
-#Base.size(b::Board) = size(b.board) # potrebuju definovat a rozsirit metodu ?
-
-
-function main()
-    
-    b = Board_init(40, 20)
+function main(x, y)
+    b = Board_init(x, y)
     fp = FigureParams(b)
     make_figure(b, fp)
 end
 
-main()
-
- 
+main(50, 50)
